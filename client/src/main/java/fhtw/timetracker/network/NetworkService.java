90ef89fd30c6d6f9e1ec6fe0012691f362cc99da
DHahn <david.gerhard.hahn@gmail.com>
2022-01-13 16:02:01 +0100

    implement methods for TimetrackerServerApi

--- client/src/main/java/fhtw/timetracker/network/NetworkService.java
+++ client/src/main/java/fhtw/timetracker/network/NetworkService.java
@@ -1,12 +1,14 @@
 package fhtw.timetracker.network;
 
-import fhtw.timetracker.model.UserDTO;
+import fhtw.timetracker.model.*;
 import okhttp3.OkHttpClient;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.Path;
 
 import java.util.List;
 
@@ -77,5 +79,209 @@ public class NetworkService {
         });
     }
 
+    public void createUser(String login, String firstname, String lastname, Role role, String passwordPlain, NetworkCallback<Void> callback) {
+        serverApi.createUser(new SignUpUser(login, firstname, lastname, role, passwordPlain)).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Void responseBody = response.body();
+                if (response.isSuccessful()) {
+                    callback.onResponse(true, responseBody, null);
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to create user HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public void deleteUser(int userId, NetworkCallback<Boolean> callback) {
+        serverApi.deleteUser(userId).enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                Boolean responseBody = response.body();
+                if (response.isSuccessful()) {
+                    callback.onResponse(true, responseBody, null);
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to create user HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public void findAllTasks(NetworkCallback<List<TaskDTO>> callback) {
+        serverApi.findAllTasks().enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<List<TaskDTO>> call, Response<List<TaskDTO>> response) {
+                if (response.isSuccessful()) {
+                    List<TaskDTO> responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to load all users, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<TaskDTO>> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public void createTask(TaskDTO taskDTO, NetworkCallback<TaskDTO> callback) {
+        serverApi.createTask(taskDTO).enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<TaskDTO> call, Response<TaskDTO> response) {
+                if (response.isSuccessful()) {
+                    TaskDTO responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to create Task, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TaskDTO> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public void deleteTask(int taskId, NetworkCallback<Boolean> callback) {
+        serverApi.deleteTask(taskId).enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                if (response.isSuccessful()) {
+                    Boolean responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to create Task, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public void findAllRecords(NetworkCallback<List<RecordDTO>> callback) {
+        serverApi.findAllRecords().enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<List<RecordDTO>> call, Response<List<RecordDTO>> response) {
+                if (response.isSuccessful()) {
+                    List<RecordDTO> responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to load all users, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<RecordDTO>> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public void findAllRecordsForUserId(int userId, NetworkCallback<List<RecordDTO>> callback) {
+        serverApi.findAllRecordsForUserId(userId).enqueue(new Callback<List<RecordDTO>>() {
+            @Override
+            public void onResponse(Call<List<RecordDTO>> call, Response<List<RecordDTO>> response) {
+                if (response.isSuccessful()) {
+                    List<RecordDTO> responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to load all users, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<RecordDTO>> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+
+    public  void createRecord(RecordDTO recordDTO, NetworkCallback<RecordDTO> callback) {
+        serverApi.createRecord(recordDTO).enqueue(new Callback<RecordDTO>() {
+            @Override
+            public void onResponse(Call<RecordDTO> call, Response<RecordDTO> response) {
+                if (response.isSuccessful()) {
+                    RecordDTO responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to create Task, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecordDTO> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
+    public void deleteRecord(int recordId, NetworkCallback<Boolean> callback) {
+        serverApi.deleteRecord(recordId).enqueue(new Callback<Boolean>() {
+            @Override
+            public void onResponse(Call<Boolean> call, Response<Boolean> response) {
+                if (response.isSuccessful()) {
+                    Boolean responseBody = response.body();
+
+                    if (responseBody != null) {
+                        callback.onResponse(true, responseBody, null);
+                    } else {
+                        callback.onResponse(false, null, new Exception("Response Body is null"));
+                    }
+                } else {
+                    callback.onResponse(false, null, new Exception("Failed to create Task, HTTP - " + response.code()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Boolean> call, Throwable t) {
+                callback.onResponse(false, null, t);
+            }
+        });
+    }
     // TODO: implement methods from TimetrackerServerApi…
 }
