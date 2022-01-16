570d69f11efa653ffae12aa26a86afa6906b83e8
izzy <misseasybella@gmail.com>
2022-01-16 19:53:35 +0100

    details controller class

---
+++ client/src/main/java/fhtw/timetracker/fxml/controller/DetailsController.java
@@ -0,0 +1,50 @@
+package fhtw.timetracker.fxml.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
+
+public class DetailsController {
+
+    @FXML
+    private Button btn_cancel;
+
+    @FXML
+    private MenuItem btn_close1;
+
+    @FXML
+    private MenuItem btn_close2;
+
+    @FXML
+    private MenuItem btn_overview;
+
+    @FXML
+    private BorderPane layoutRoot;
+
+    @FXML
+    private Label lbl_timeCalc;
+
+    @FXML
+    void cancelButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeApp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showOverview(ActionEvent event) {
+
+    }
+
+}
